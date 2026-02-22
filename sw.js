873f6e8e8751b98387fa87a235a89e8fcff8c3bd
sergiosabalete93-lang <sergiosabalete93@gmail.com>
2026-02-22 15:42:57 +0000

    Update and rename worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+// sw.js
+const CACHE_NAME = 'payroll-pro-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devuelve el archivo de la caché si existe, si no, lo pide a internet
+        return response || fetch(event.request);
+      })
+  );
+});
